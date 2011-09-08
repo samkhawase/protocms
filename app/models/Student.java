58e9b0b9f3dc5e708be58ed25c270fbd642801ae
yantramanav <sam.khawse@gmail.com>
2011-09-08 15:42:59 -0500

    9/8/2011 EOD commit

---
+++ app/models/Student.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.util.Date;
+
+import play.data.validation.Required;
+import play.modules.morphia.Model;
+
+
+/* this class has Student related details */
+
+public class Student extends Model {
+	
+	// User details, user details are fetched as Student.user.userId; 
+	@Required
+	public User user;
+	
+	// personalDetails
+	public String firstName;
+	public String middleName;
+	public String lastName;
+	public String addressLine1;
+	public String addressLine2;
+	public String city;
+	public String state;
+	public String zipCode;
+	public int phoneNumber;
+	public Date dateofBirth;
+	public String sex;
+	public String bloodGroup;
+	
+	// enrolment details
+	public long enrolmentNumber;
+	public int rollNumber; 			// can be String also
+
+	// Course details
+	public String major;
+	public String[] subjects;
+	public String currentClass;
+	
+
+}
---
+++ app/models/Teacher.java
@@ -0,0 +1,7 @@
+package models;
+
+import play.modules.morphia.Model;
+
+public class Teacher extends Model {
+
+}
